5b41add28ac518b3d04372de8ce289f24a89e962
gabrielabud <gabriela.budeanu@gmail.com>
2017-10-03 23:07:09 +0300

    chap09 ex2

---
+++ chap09/ex2.rb
@@ -0,0 +1,11 @@
+def old_roman_numeral num
+old_roman=""
+old_roman=old_roman+"M"*(num/1000)
+old_roman=old_roman+"D"*(num%1000/500)
+old_roman=old_roman+"C"*(num%500/100)
+old_roman=old_roman+"L"*(num%100/50)
+old_roman=old_roman+"X"*(num%50/10)
+old_roman=old_roman+"V"*(num%10/5)
+old_roman=old_roman+"I"*(num%5)
+end
+puts(old_roman_numeral 3050)
