9369830372e40b5b7c702037f1fbefb247e6d558
gabrielabud <gabriela.budeanu@gmail.com>
2017-10-03 10:09:42 +0300

    Chap05 ex1

---
+++ chap05/ex1.rb
@@ -0,0 +1,8 @@
+#!/Users/Gabriela/.rvm/rubies/ruby-2.4.1/bin/ruby
+puts "First_name"
+First_name=gets.chomp
+puts "Middle_name"
+Middle_name=gets.chomp
+puts "Last_name"
+Last_name=gets.chomp
+puts "Hello"+" "+First_name+" "+Middle_name+" "+Last_name
