f1fb0c47134448a8afb4d011751ee981d4a88615
gabrielabud <gabriela.budeanu@gmail.com>
2017-10-09 00:35:50 +0300

    Chap13 ex2_old_roman

--- chap13/ex2.rb
+++ chap13/ex2.rb
@@ -6,6 +6,19 @@ class Integer
       self*(self-1).factorial
     end
   end
+
+  def old_roman_numeral
+  old_roman=""
+  old_roman=old_roman+"M"*(self/1000)
+  old_roman=old_roman+"D"*(self%1000/500)
+  old_roman=old_roman+"C"*(self%500/100)
+  old_roman=old_roman+"L"*(self%100/50)
+  old_roman=old_roman+"X"*(self%50/10)
+  old_roman=old_roman+"V"*(self%10/5)
+  old_roman=old_roman+"I"*(self%5)
+  end
+
 end
 
 puts 5.factorial
+puts 52.old_roman_numeral
